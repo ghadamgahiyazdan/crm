d0b4774b71bbe74a553312dd41a7cfea30c8f478
ghadamgahiyazdan <ghadamgahiyazdan@gmail.com>
2025-05-22 13:33:05 +0330

    add otp add mobile Home page

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,3 +1,4 @@
+import HomeMobile from "@/components/custom/home/mobile/HomeMobile";
 import HomePc from "@/components/custom/home/pc/HomePc";
 
 const Home = () => {
@@ -5,6 +6,9 @@ const Home = () => {
     <main className="min-h-screen bg-white">
       <section className="hidden lg:block">
         <HomePc/>
+      </section>
+      <section className="lg:hidden">
+        <HomeMobile/>
       </section>
     </main>
   );
---
+++ src/components/custom/home/mobile/HomeMobile.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeMobile = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default HomeMobile;
